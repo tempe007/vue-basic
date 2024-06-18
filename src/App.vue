344b093e8edafddce781c97548b115f9364dd2db
신지호-플그림 <shinjiho@sinjihoui-noteubug-2.local>
2024-06-18 09:30:29 +0900

    basic vue-cli,vue-form commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<form v-on:submit.prevent="submitForm">
+  <div>
+    <label for="username">id : </label>
+    <input id="username" v-model="username" type="text" placeholder="아이디">
+  </div>
+  <div>
+    <label for="password">pw : </label>
+    <input id="password" v-model="password" type="password" placeholder="패스워드">
+  </div>
+  <button type = "submit">login</button>
+</form>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      username : '',
+      password : ''
+    }
+  },
+  methods : {
+    submitForm : function () {
+      //e.preventDefault();
+      let url = 'https://jsonplaceholder.typicode.com/users';
+      let data = {
+        username : this.username,
+        password : this.password
+      }
+      axios.post(url,data).then(function (response) {
+        console.log(response);
+      }).catch(function (error) {
+        console.log(error);
+      });
+
+    }
   }
+
 }
 </script>
 
